290d83d270c45f4232fca4729c2f0d90f2c4f0c1
Louis Chung <louis@light-has-come.org>
2025-12-04 23:51:50 +0800

    chore: 強制重新部署 - Footer 動態資訊更新

--- app/version.ts
+++ app/version.ts
@@ -1,2 +1,8 @@
-// Version: 2024-12-04-23:40 - Footer sync update
-export { }
+// 版本資訊 - 用於追蹤部署
+export const VERSION = '1.0.1'
+export const BUILD_DATE = '2024-12-04'
+export const CHANGES = [
+    'Footer 統一使用動態 CMS 資訊',
+    '活動花絮頁面 Footer 改為讀取聯絡資訊',
+    '課程介紹頁面使用共用 Footer 組件'
+]
